f240a3ff09c623e4fc4f816329f390fe83f58c06
Genius Micheal <oluwasegunmicheal10@gmail.com>
2022-10-14 20:58:57 +0100

    WIP

--- src/pages/public/Home/NewLetter.tsx
+++ src/pages/public/Home/NewLetter.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 const NewLetter:React.FC  = () => {
    return <div className="newLetter">
-      <section className="relative flex items-center">
-         <div className="flex">
+      <section className="flex">
+         <div className="flex" m-auto>
             <h3>Subscribe our Newsletter</h3>
             <span>Get <b className="text-white">25 persent Off</b> first purchase!</span>
          </div>
-         <form action="" method="post"></form>
+         <form action="" method="post" className="flex">
+            <input type="text" placeholder="Your email address"/>
+            <input type="submit" value="Subscribe"/>
+         </form>
       </section>
    </div>
 }
